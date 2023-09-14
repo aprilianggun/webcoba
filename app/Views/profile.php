7c823d9999431ed30f4eae99b99ce86ecbf3ee4e
aprilianggun <aprilia.anggun21@students.unila.ac.id>
2023-09-14 22:04:06 +0700

    update

---
+++ app/Views/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Profile Page</h1>
+    <p>Nama: <?= $nama ?></p>
+    <p>Kelas: <?= $kelas ?></p>
+    <p>NPM: <?= $npm ?></p>
+</body>
+
+</html>
