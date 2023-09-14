d481e405e1ef7a1f8944ad0a154cf17de926d283
aprilianggun <aprilia.anggun21@students.unila.ac.id>
2023-09-14 22:56:45 +0700

    profile

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -4,14 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/biodata.css">
+    <link rel="stylesheet" href="/biodata.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Profile Page</h1>
-    <p>Nama: <?= $nama ?></p>
-    <p>Kelas: <?= $kelas ?></p>
-    <p>NPM: <?= $npm ?></p>
+    <div class="background">
+        <img src="/april.jpeg" alt="cantik" width=130 height=130 class="gambar">
+        <div class="teks">
+            <p>Nama: <?= $nama ?></p>
+            <p>Kelas: <?= $kelas ?></p>
+            <p>NPM: <?= $npm ?></p>
+        </div>
+    </div>
 </body>
 
 </html>
